b95fcc2a668992f66595e36e2b9115d2a6419f4a
Mandar Limaye <mandarl@users.noreply.github.com>
2024-06-29 14:08:10 -0500

    Remove un-necessary provides

--- ui/src/main/java/org/dallasmakerspace/thymeleaf/server/di/ServerModule.kt
+++ ui/src/main/java/org/dallasmakerspace/thymeleaf/server/di/ServerModule.kt
@@ -2,14 +2,10 @@ package org.dallasmakerspace.thymeleaf.server.di
 
 import dagger.Module
 import dagger.Provides
-import org.dallasmakerspace.thymeleaf.server.auth.UserInfoProvider
 import org.dallasmakerspace.thymeleaf.server.common.DMSHttpClient
 
 @Module
 class ServerModule {
 
   @Provides fun provideHttpClient() = DMSHttpClient()
-
-  @Provides
-  fun provideUserInfoProvider(dmsHttpClient: DMSHttpClient) = UserInfoProvider(dmsHttpClient)
 }
