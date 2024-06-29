66c9280e087172a1afb4f997311a826842b07e44
Mandar Limaye <mandarl@users.noreply.github.com>
2024-06-29 13:46:54 -0500

    Add details to error

--- ui/src/main/java/org/dallasmakerspace/thymeleaf/server/plugins/StatusPages.kt
+++ ui/src/main/java/org/dallasmakerspace/thymeleaf/server/plugins/StatusPages.kt
@@ -26,7 +26,7 @@ fun Application.configureStatusPages() {
               "error5xx",
               mapOf(
                   "message" to "There was an error processing your request.",
-                  "cause" to "$cause",
+                  "cause" to "$cause: ${cause.stackTraceToString()}",
                   "sso_profile_url" to ssoProfileUrl)))
     }
   }
