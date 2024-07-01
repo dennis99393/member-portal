13b6c4e93b2323d0f669cb507afd45c402a3819d
Mandar Limaye <mandarl@users.noreply.github.com>
2024-06-30 19:06:34 -0500

    Refactor link discourse handler

--- ui/src/main/java/org/dallasmakerspace/thymeleaf/server/discourse/LinkDiscourseHandler.kt
+++ ui/src/main/java/org/dallasmakerspace/thymeleaf/server/discourse/LinkDiscourseHandler.kt
@@ -2,15 +2,11 @@ package org.dallasmakerspace.thymeleaf.server.discourse
 
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.sessions.*
+import org.dallasmakerspace.thymeleaf.server.auth.UserInfoProvider
+import org.dallasmakerspace.thymeleaf.server.plugins.AuthException
+import org.dallasmakerspace.thymeleaf.server.routes.AuthRouteHandler
 import javax.inject.Inject
 import kotlin.random.Random
-import org.dallasmakerspace.thymeleaf.server.auth.UserInfoProvider
-import org.dallasmakerspace.thymeleaf.server.common.Log
-import org.dallasmakerspace.thymeleaf.server.plugins.AuthException
-import org.dallasmakerspace.thymeleaf.server.plugins.UserSession
-import org.dallasmakerspace.thymeleaf.server.routes.IRouteHandler
-import org.dallasmakerspace.thymeleaf.server.routes.RouteFactory
 
 private const val NONCE_RANGE_START = 1000000
 private const val NONCE_RANGE_END = 9999999
@@ -21,28 +17,15 @@ constructor(
     private val userInfoProvider: UserInfoProvider,
     private val discourseNonceCache: DiscourseNonceCache,
     private val discourseLinkProvider: DiscourseSSOProvider
-) : IRouteHandler {
-
+) : AuthRouteHandler(userInfoProvider) {
   override suspend fun handle(call: ApplicationCall) {
-    val session = call.sessions.get<UserSession>()
-    val accessToken = session?.accessToken
-    if (accessToken != null) {
-      val jsonMap: Map<String, Any>?
-
-      try {
-        jsonMap = userInfoProvider.getUserInfo(accessToken).toMutableMap()
-        val username =
-            checkNotNull(jsonMap["preferred_username"] as String?) { "No username in userinfo" }
-        val randomNonce = Random.nextInt(NONCE_RANGE_START, NONCE_RANGE_END).toString()
-        discourseNonceCache.setNonce(username, randomNonce)
-        val discourseSSOConnectUrl = discourseLinkProvider.getDiscourseSSOConnectUrl(randomNonce)
-        call.respondRedirect(discourseSSOConnectUrl, permanent = false)
-        return
-      } catch (e: AuthException) {
-        call.sessions.clear<UserSession>()
-        Log.e("Failed to get user info", e)
-      }
-    }
-    call.respondRedirect(RouteFactory.Paths.INDEX.path, permanent = false)
+    super.handle(call)
+    val username =
+        userInfo["preferred_username"] as? String
+            ?: throw AuthException("No username found in user info")
+    val randomNonce = Random.nextInt(NONCE_RANGE_START, NONCE_RANGE_END).toString()
+    discourseNonceCache.setNonce(username, randomNonce)
+    val discourseSSOConnectUrl = discourseLinkProvider.getDiscourseSSOConnectUrl(randomNonce)
+    call.respondRedirect(discourseSSOConnectUrl, permanent = false)
   }
 }
