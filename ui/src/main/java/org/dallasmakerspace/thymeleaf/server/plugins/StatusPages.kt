328f9102fc6f4414850db47d3b6c94e4d20c3096
Mandar Limaye <mandarl@users.noreply.github.com>
2024-08-21 14:37:34 -0500

    Add session and user to error page

--- ui/src/main/java/org/dallasmakerspace/thymeleaf/server/plugins/StatusPages.kt
+++ ui/src/main/java/org/dallasmakerspace/thymeleaf/server/plugins/StatusPages.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respond
+import io.ktor.server.sessions.*
 import io.ktor.server.thymeleaf.ThymeleafContent
 import org.dallasmakerspace.thymeleaf.server.auth.getOAuthSettings
 import org.dallasmakerspace.thymeleaf.server.di.DaggerAppComponent
@@ -20,6 +21,7 @@ fun Application.configureStatusPages() {
           ThymeleafContent("error404", mapOf("message" to "Sorry! Page was not found.")))
     }
     exception<Throwable> { call, cause ->
+      val session = call.sessions.get<UserSession>()
       call.respond(
           status = HttpStatusCode.InternalServerError,
           ThymeleafContent(
@@ -27,6 +29,8 @@ fun Application.configureStatusPages() {
               mapOf(
                   "message" to "There was an error processing your request. ${cause.message}.",
                   "cause" to "$cause:\n ${cause.stackTraceToString()}",
+                  "sessionid" to session?.sessionId as String,
+                  "userid" to session.userId as String,
                   "sso_profile_url" to ssoProfileUrl)))
     }
   }
