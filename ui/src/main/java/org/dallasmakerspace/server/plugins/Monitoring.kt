2b457ba8da2041778c0792dc89f817f5f7bfe354
Mandar Limaye <mandarl@users.noreply.github.com>
2025-01-20 16:06:57 -0600

    Temp: log static routes to debug issues

--- ui/src/main/java/org/dallasmakerspace/server/plugins/Monitoring.kt
+++ ui/src/main/java/org/dallasmakerspace/server/plugins/Monitoring.kt
@@ -20,7 +20,7 @@ fun Application.configureMonitoring() {
   install(CallLogging) {
     level = Level.INFO
     logger = loggerFactory.create(Application::class.java)
-    filter { call -> call.request.path().startsWith("/static").not() } // Don't log static
+    // filter { call -> call.request.path().startsWith("/static").not() } // Don't log static
 
     format { call ->
       val status = call.response.status()
