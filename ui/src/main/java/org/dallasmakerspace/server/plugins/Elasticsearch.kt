bb162a6c88c6b2913e8930844450a315f94f929a
Mandar Limaye <mandarl@users.noreply.github.com>
2025-01-26 15:56:32 -0600

    Temp: disable ES Logging

--- ui/src/main/java/org/dallasmakerspace/server/plugins/Elasticsearch.kt
+++ ui/src/main/java/org/dallasmakerspace/server/plugins/Elasticsearch.kt
@@ -1,11 +1,9 @@
 package org.dallasmakerspace.server.plugins
 
 import io.ktor.server.application.*
-import org.dallasmakerspace.server.common.logging.ElasticsearchClientManager
-import org.dallasmakerspace.server.common.logging.logToElasticsearch
 
 fun Application.configureElasticsearch() {
   intercept(ApplicationCallPipeline.Monitoring) {
-    logToElasticsearch(ElasticsearchClientManager.client)
+    // logToElasticsearch(ElasticsearchClientManager.client)
   }
 }
